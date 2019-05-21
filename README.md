2752f1a3ab0ef3df0d081fe93d8066e45c078bb2
jp <jonahphilion@college.harvard.edu>
2019-05-20 20:39:56 -0400

    add cvpr 2019

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# cvpr_2019_with_code
-Rank the papers accepted to CVPR 2019 by the number of stars in their github repository.
+# CVPR with code
 
-### Thank you
 Made possible by this list of accepted CVPR papers https://github.com/hoya012/CVPR-2019-Paper-Statistics/tree/master/2019_cvpr and this wonderful website https://paperswithcode.com/
---
+++ scrape.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import requests
+from selenium import webdriver
+from time import sleep
+from tqdm import tqdm
+
+def get_info(df, present_type):
+    info = []
+    for namei,name in enumerate(tqdm(df['Paper Title'])):
+        success = False
+        url = 'https://paperswithcode.com/search?q={}'.format(name.replace(' ', '+'))
+        # hangs for some reason...
+        if url == 'https://paperswithcode.com/search?q=Pluralistic+Image+Completion':
+            info.append([name, present_type, 'https://github.com/lyndonzheng/Pluralistic-Inpainting', 132])
+            continue
+        driver.get(url)
+        value = driver.find_element_by_class_name('infinite-container')
+        items = value.find_elements_by_class_name('infinite-item')
+        for item in items[:1]:
+            badge = item.find_element_by_class_name('badge-dark')
+            if badge.text == 'Code':
+                badge.click()
+                impls = driver.find_element_by_id('id_paper_implementations_collapsed')
+                github_link = impls.find_element_by_class_name('code-table-link').get_attribute('href')
+                stars = int(impls.text.split('\n')[1].replace(',', ''))
+                info.append([name, present_type, github_link, stars])
+                success = True
+        if not success:
+            info.append([name, present_type, '', 0])
+    return info
+
+driver = webdriver.Chrome('/Users/jonahphilion/Downloads/chromedriver')  # Optional argument, if not specified will search path.
+info = []
+
+df_oral = pd.read_csv('cvpr_2019_oral.csv')
+df_poster = pd.read_csv('cvpr_2019_poster.csv')
+df_poster = df_poster[~df_poster['Paper Title'].isin(set(df_oral['Paper Title']))]
+
+info.extend(get_info(df_poster, 'Poster'))
+info.extend(get_info(df_oral, 'Oral'))
+driver.quit()
+
+out_df = pd.DataFrame(info)
+out_df.columns = ['name', 'presentation type', 'github link', 'stars']
+out_df = out_df.sort_values(['stars', 'presentation type'], ascending=False)
+out_df.index = range(1, len(out_df) + 1)
+outname = 'cvpr_2019_github_links.csv'
+print('saving', outname)
+out_df.to_csv(outname)
+
+
